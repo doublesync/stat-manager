36f152537062f127f4d4d51fccfd28aba23943a2
christopher.carlton <chriscarlton368@gmail.com>
2024-02-12 22:53:56 -0500

    Updated DiscordUser model (type annotations).

--- core/models.py
+++ core/models.py
@@ -3,19 +3,19 @@ from .managers import DiscordAuthorizationManager
 
 # Create your models here.
 class DiscordUser(models.Model):
-    objects = DiscordAuthorizationManager()
-    id = models.BigIntegerField(primary_key=True, serialize=False)
-    discord_tag = models.CharField(max_length=100)
-    avatar = models.CharField(max_length=100)
-    public_flags = models.IntegerField()
-    flags = models.IntegerField()
-    locale = models.CharField(max_length=100)
-    mfa_enabled = models.BooleanField()
-    last_login = models.DateTimeField(null=True)
-    last_reward = models.DateTimeField(null=True)
+    objects: any = DiscordAuthorizationManager()
+    id: int = models.BigIntegerField(primary_key=True, serialize=False)
+    discord_tag: str = models.CharField(max_length=100)
+    avatar: str = models.CharField(max_length=100)
+    public_flags: int = models.IntegerField()
+    flags: int = models.IntegerField()
+    locale: str = models.CharField(max_length=100)
+    mfa_enabled: bool = models.BooleanField()
+    last_login: str = models.DateTimeField(null=True)
+    last_reward: str = models.DateTimeField(null=True)
 
-    def is_authenticated(self, request):
+    def is_authenticated(self, request) -> bool:
         return True
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.discord_tag}"
