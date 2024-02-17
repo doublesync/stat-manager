8b05192dd05256d27087f9d7affcdb7dbee3d8e0
christopher.carlton <chriscarlton368@gmail.com>
2024-02-17 11:23:46 -0500

    Added JSONEditor to admin panel.

--- basketball/admin.py
+++ basketball/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-
+from django.db.models.fields.json import JSONField
+from jsoneditor.forms import JSONEditor
 from .models import BasketballPlayer, BasketballTeam
 
+
+# Custom admin class
+class myAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        JSONField: {"widget": JSONEditor},
+    }
+
+
 # Register your models here.
-admin.site.register(BasketballPlayer)
-admin.site.register(BasketballTeam)
+admin.site.register(BasketballPlayer, myAdmin)
+admin.site.register(BasketballTeam, myAdmin)
