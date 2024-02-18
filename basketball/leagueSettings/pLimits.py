1dc9964e1baf0a2776d8362bcf1797072ffa840c
christopher.carlton <chriscarlton368@gmail.com>
2024-02-17 20:50:34 -0500

    Corrected typo'd height limits.

--- basketball/leagueSettings/pLimits.py
+++ basketball/leagueSettings/pLimits.py
@@ -28,23 +28,23 @@ playerLimits: dict[str, dict[str, int]] = {
     "Athletic": defaultPlayerLimits,
     "Giant": {
         "PG": {
-            "heightLimits": [75, 79],
+            "heightLimits": [76, 79],
             "weightLimits": [160, 200],
         },
         "SG": {
-            "heightLimits": [76, 80],
+            "heightLimits": [77, 80],
             "weightLimits": [180, 220],
         },
         "SF": {
-            "heightLimits": [78, 82],
+            "heightLimits": [79, 82],
             "weightLimits": [200, 240],
         },
         "PF": {
-            "heightLimits": [80, 84],
+            "heightLimits": [81, 84],
             "weightLimits": [220, 260],
         },
         "C": {
-            "heightLimits": [83, 91],
+            "heightLimits": [84, 91],
             "weightLimits": [240, 300],
         },
     },
