445effd4f539ee7353a6dbbc444488d4f3c0b1e8
christopher-carlton <80734143+doublesync@users.noreply.github.com>
2024-02-19 12:11:47 -0500

    Added proficiency list, proficiency check function and attribute price check function.

--- basketball/leagueSettings/pAttributes.py
+++ basketball/leagueSettings/pAttributes.py
@@ -1,3 +1,5 @@
+from basketabll.leagueSettings.pHeight import realRange
+
 # Starting attributes depending on the players build will live here.
 physicalAttributes: list = [
     "Speed",
@@ -6,6 +8,7 @@ physicalAttributes: list = [
     "Strength",
     "Speed with Ball",
 ]
+
 startingAttributes: dict[str, dict[str, int]] = {
     "PG": {
         "Driving Layup": 75,
@@ -242,3 +245,47 @@ attributeCategories: dict[str, list[str]] = {
 }
 
 # Starting attributes are set in pPhysical.py [!]
+
+attributePrices: dict[str, dict[range, int]] = {
+    "proficient": {
+        realRange(0, 74): 1,
+        realRange(75, 79): 2,
+        realRange(80, 84): 4,
+        realRange(85, 89): 5,
+        realRange(90, 94): 8,
+        realRange(95, 99): 10,
+    },
+    "normal": {
+        realRange(0, 64): 1,
+        realRange(65, 74): 2,
+        realRange(75, 79): 4,
+        realRange(80, 84): 5,
+        realRange(85, 89): 7,
+        realRange(90, 94): 10,
+        realRange(95, 99): 11,
+    }
+}
+
+attributeProficiencies: dict[str, list[str]] = {
+  "Giant": [],
+  "Skilled": [],
+  "Athletic": [],
+}
+
+def checkProficiency(archetype: str, attribute: str) -> str:
+  proficiency: list = attributeProficiencies[archetype]
+  if attribute in proficiency:
+    return "Proficient"
+  else:
+    return "Normal"
+
+def checkAttributePrice(archetype: str, attribute: str, startLevel: int, endLevel: int) -> int:
+  cost: int = 0
+  proficiency: str = checkProficiency(archetype, attribute)
+  for i in range(startLevel + 1, endLevel + 1):
+    for priceRange in attributePrices[proficiency]:
+      if i in priceRange:
+        print(f"{i} will cost user {attributePrices[proficiency][priceRange]}")
+        cost += attributePrices[proficiency][priceRange]
+        continue
+  return cost
